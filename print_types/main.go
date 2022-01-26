2bd4bf7004e6ba4b5b41f3a4f479fb2d5435629c
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2022-01-26 22:38:21 +0300

    print types added

---
+++ print_types/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World")
+	fmt.Println("Hello World")
+	fmt.Printf("Hello World")
+	//println yeni satıra geçerek basar, print aynı satıra basar printf'e de string verirsek direkt basar
+
+	name := "Deniz"
+
+	fmt.Print(name)
+	fmt.Println(name)
+	fmt.Printf(name)
+
+	fmt.Print("Benim adım:", name, "\n")
+	fmt.Println("Benim adım:", name)
+	fmt.Printf("Benim adım:", name)
+
+	/*
+		Benim adım:Deniz
+		Benim adım: Deniz
+		Benim adım: %!(EXTRA string=Deniz)
+	*/
+
+	fmt.Printf("Benim adım: %v", name) // değeri bastırır
+	fmt.Printf("Benim adım: %T", name) // tipi yazdırır
+	fmt.Printf("Benim adım: %v %T", name, name)
+	// %d 10 luk tabanda gösterir %c char ama %v herşeyle kullanılır
+
+	//VISIBILITY --> scope mantığı
+
+	//NAMING
+	/*
+		Sade ve anlaşılır olmalı
+		camelCase kullanılır.
+		Kısatmalar büyük harfle URL gibi Url değil
+		Başka değişkenadında da büyük myHTTP gibi
+		for dögüsü indexi için i,j,k olabilir
+	*/
+
+}
