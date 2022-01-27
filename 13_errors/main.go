2527c5ffd82ebc0a839f222848442f6a057bd602
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2022-01-27 22:57:33 +0300

    exercises added

--- 13_errors/main.go
+++ 13_errors/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -69,3 +72,77 @@ func main() {
 func sRoot(num float64) float64 {
 	return math.Sqrt(num)
 } */ //NaN döner,beklenmeyen sonuç bir hatadır.
+
+/*
+*************************************************** */
+//EXERCISES
+
+/*func main() {
+	fmt.Print("Lütfen aldığınız notu giriniz: ")
+	grade, _ := getGrade()
+	var result string
+	if grade >= 50 {
+		result = "Geçtin"
+	} else {
+		result = "Kaldın"
+	}
+	fmt.Println(result)
+}*/
+
+func getGrade() (int, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	input = strings.TrimSpace(input)
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return num, nil
+}
+
+/*
+func main() {
+
+	target := numRand(1, 100)
+
+	fmt.Println("1 ile 100 arasındaki sayıyı bulmaya çalışın")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for attempts := 0; attempts < 10; attempts++ {
+		fmt.Println(10-attempts, "hakkınız kaldı")
+		fmt.Print("Lütfen tahmininizi yapınız: ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		input = strings.TrimSpace(input)
+		num, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if num > target {
+			fmt.Println("Tahmininiz daha büyük, daha küçük bir sayı giriniz.")
+		} else if num < target {
+			fmt.Println("Tahmininiz daha küçük, daha büyük bir sayı giriniz.")
+		} else {
+			fmt.Println("Doğru Tahmin, hedf sayı ", target, " idi ", attempts, " seferde bulundunuz")
+			break
+		}
+
+	}
+
+}
+
+func numRand(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+*/
